a14bb41dccd84c0d0e535c1de07cb2c006ce4ca4
andyb0024 <andy.benage0024@gmail.com>
2021-07-20 06:18:51 -0700

    comment reply

--- Comments/serializers.py
+++ Comments/serializers.py
@@ -1,7 +1,31 @@
 from rest_framework import serializers
 from.models import Comment
 class CommentSerializer(serializers.ModelSerializer):
+    replies_count=serializers.SerializerMethodField()
+    class Meta:
+        model=Comment
+        fields=['id','content_type','object_id','parent','body','replies_count']
+
+    def get_replies_count(self,obj):
+        if obj.is_parent:
+            return obj.children().count()
+        return 0
+
+class CommentChildSerializer(serializers.ModelSerializer):
 
     class Meta:
         model=Comment
         fields=['id','content_type','object_id','parent','body']
+        fields=['id','timestamp','body']
+
+
+class CommentDetailSerializer(serializers.ModelSerializer):
+    replies=serializers.SerializerMethodField()
+    class Meta:
+        model=Comment
+        fields=['id','content_type','object_id','parent','body','replies','timestamp']
+
+    def get_replies(self,obj):
+        if obj.is_parent:
+            return CommentChildSerializer(obj.children(),many=True).data
+        return None
--- Comments/urls.py
+++ Comments/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from.views import CommentListView,CommentDetailApiView
 urlpatterns = [
    path('', CommentListView.as_view(), name='list'),
-   path('comment-list/<int:pk>/', CommentDetailApiView.as_view(), name='detail'),
+   path('<int:pk>/', CommentDetailApiView.as_view(), name='detail'),
 
 ]
